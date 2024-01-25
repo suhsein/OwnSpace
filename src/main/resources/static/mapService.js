b59ee336e06a58c466c19ce2e835961b31f2b29b
suhsein <mini9075@naver.com>
2024-01-26 00:27:11 +0900

    일정 등록 장소 선택 기능 중간 완료. 검색방식으로 수정 요함

--- src/main/resources/static/mapService.js
+++ src/main/resources/static/mapService.js
@@ -212,16 +212,27 @@ function removeAllChildNodes(el) {
 // element 내용 복사
 function copy(id, msg) {
     window.navigator.clipboard.writeText(id.textContent).then(() => {
-        alert(msg + " 복사완료");
+        alert(msg + checkKorean(msg) + " 복사되었습니다.");
     });
 }
 
+function checkKorean(str) {
+    const lastChar = str.charCodeAt(str.length - 1)
+    const isThereLastChar = (lastChar - 0xac00) % 28
+    if (isThereLastChar) {
+        return '이'
+    }
+    return '가'
+}
+
 // menu visibility
-function menuVisibility(id) {
+function menuVisibility($menuControl) {
     $menuWrap = document.getElementById("menu_wrap");
     if ($menuWrap.style.visibility !== 'hidden') {
+        $menuControl.innerHTML = '목록 열기';
         $menuWrap.style.visibility = 'hidden';
     } else {
+        $menuControl.innerHTML = '목록 닫기';
         $menuWrap.style.visibility = 'visible';
     }
 }
